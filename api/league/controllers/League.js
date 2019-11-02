accd9342df3c4705c73a5200aa4ddf3668de3fae
Tomasz Kaminski <>
2019-11-02 13:08:44 +0100

    league signing

--- api/league/controllers/League.js
+++ api/league/controllers/League.js
@@ -72,8 +72,67 @@ module.exports = {
     }
 
     let rawdata = fs.readFileSync(callForLeagueDataFile);
-    let comingLeagues = JSON.parse(rawdata);
-    ctx.send(comingLeagues);
+    let json = JSON.parse(rawdata);
+    let jsonArray = getValues(json.coming_leagues);
+    let result = [];
+
+    jsonArray.forEach(league => {
+      delete league.participants;
+      result.push(league);
+    });
+    ctx.send(result);
+  },
+
+  joinLeague: async ctx => {
+    if (!fs.existsSync(callForLeagueDataFile)) {
+      fs.writeFileSync(
+        callForLeagueDataFile,
+        JSON.stringify({
+          coming_leagues: []
+        })
+      );
+    }
+
+    let rawdata = fs.readFileSync(callForLeagueDataFile);
+    let callForLeagueData = JSON.parse(rawdata);
+
+    let jsonBody = ctx.request.body;
+    console.log(jsonBody.league);
+
+    if (
+      !(
+        jsonBody.hasOwnProperty("league") &&
+        callForLeagueData.coming_leagues[jsonBody.league] != null
+      )
+    ) {
+      ctx.send({
+        isValid: false,
+        error: "Zły identyfikator ligi."
+      });
+      return;
+    }
+
+    let validatedData = validateJoinLeagueData(
+      jsonBody,
+      callForLeagueData.coming_leagues[jsonBody.league].participants,
+      callForLeagueData.coming_leagues[jsonBody.league].signingLimitDate
+    );
+
+    if (validatedData.isValid) {
+      callForLeagueData.coming_leagues[jsonBody.league].participants.push({
+        username: jsonBody.nickname,
+        email: jsonBody.email,
+        apiKey: jsonBody.apiKey,
+        apiSecret: jsonBody.apiSecret
+      });
+
+      fs.writeFileSync(
+        callForLeagueDataFile,
+        JSON.stringify(callForLeagueData)
+      );
+    }
+
+    ctx.send(validatedData);
   },
 
   index: async ctx => {
@@ -100,6 +159,70 @@ module.exports = {
     }
   }
 };
+
+function validateKeys(key, secret) {
+  return true;
+}
+
+function hashSecret(secret) {
+  return secret;
+}
+
+function isNullOrEmpty(data) {
+  console.log(data);
+  return data === undefined || data === null || data === "";
+}
+
+function validateJoinLeagueData(data, participants, signingLimitDate) {
+  if (
+    isNullOrEmpty(data.apiSecret) ||
+    isNullOrEmpty(data.apiKey) ||
+    isNullOrEmpty(data.nickname) ||
+    isNullOrEmpty(data.email)
+  ) {
+    return {
+      isValid: false,
+      error: "Nieprawidłowe dane."
+    };
+  }
+  let now = new Date();
+  if (new Date(signingLimitDate) < now) {
+    return {
+      isValid: false,
+      error: "Zapisy na wybraną ligę są zakończone."
+    };
+  }
+
+  if (validateKeys(data.apiKey, data.apiSecret) === false) {
+    return {
+      isValid: false,
+      error: "Podane klucze API są nieprawidłowe."
+    };
+  }
+
+  let hashedApiSecret = hashSecret(data.apiSecret);
+
+  if (
+    participants.find(item => {
+      return (
+        item.apiKey === data.apiKey ||
+        item.hashedApiSecret === hashedApiSecret ||
+        item.email === data.email ||
+        item.username === data.nickname
+      );
+    })
+  ) {
+    return {
+      isValid: false,
+      error: "Niektóre dane powielają się z juz zapisanym uczestnikiem."
+    };
+  }
+
+  return {
+    isValid: true,
+    error: null
+  };
+}
 
 function getValues(obj) {
   var values = [];
